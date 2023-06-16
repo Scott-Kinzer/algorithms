ff37e531e4ad1e83a9558e8a555af15f83dd6311
Scott-Kinzer <zaz765831@gmail.com>
2023-06-16 13:54:51 +0300

    fix: refactored files structure

--- run.js
+++ run.js
@@ -5,27 +5,41 @@ const { exec } = require('child_process');
 // Get the current directory
 const directoryPath = __dirname + '/build';
 
+const runFiles = (err, files, directory) => {
+if (err) {
+  console.log('Error reading directory:', err);
+  return;
+}
+
+// Filter out only JavaScript files
+const jsFiles = files.filter(file => file.endsWith('.js'));
+
+// Run each JavaScript file
+jsFiles.forEach(file => {
+  exec(`node ./build/${directory}/${file}`, (error, stdout) => {
+    if (error) {
+      console.error(`Error running file: ${file}\n`, error);
+    } else {
+      console.log('__________________________________');
+      console.log(file);
+      console.log(stdout);
+    }
+  });
+});
+};
+
 // Read all files in the directory
 fs.readdir(directoryPath, (err, files) => {
-    console.log(files);
   if (err) {
-    console.log('Error reading directory:', err);
+    console.log('Directory issue');
     return;
   }
 
-  // Filter out only JavaScript files
-  const jsFiles = files.filter(file => file.endsWith('.js'));
+  const outerDirectories = files;
 
-  // Run each JavaScript file
-  jsFiles.forEach(file => {
-    exec(`node ./build/${file}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error running file: ${file}\n`, error);
-      } else {
-        console.log('__________________________________');
-        console.log(file);
-        console.log(stdout);
-      }
-    });
-  });
+  fs.readdir(directoryPath + '/' + outerDirectories[0], 
+  (err, files) => runFiles(err, files, outerDirectories[0]));
+
+  fs.readdir(directoryPath + '/' + outerDirectories[1], 
+  (err, files) => runFiles(err, files, outerDirectories[1]));
 });
