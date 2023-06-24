6de63594e2a684ec792fa65b7df4228ebece8213
Scott-Kinzer <zaz765831@gmail.com>
2023-06-24 17:50:45 +0300

    feat: added search binary tree method

--- src/data-structures/binary.three.ts
+++ src/data-structures/binary.three.ts
@@ -49,6 +49,20 @@ class BinaryTree {
       this.insertNode(root.right, newNode);
     }
   }
+
+  search(root: Node | null, value: number): Node | null {
+    if (!root) return null;
+
+    if (root.value === value) {
+      return root;
+    }
+
+    if (value < root.value) {
+      return this.search(root.left, value);
+    } else {
+      return this.search(root.right, value);
+    }
+  }
 }
 
 const binary = new BinaryTree();
@@ -56,3 +70,8 @@ const binary = new BinaryTree();
 binary.insert(5);
 binary.insert(2);
 binary.insert(7);
+
+console.log(binary.search(binary.root, 5));
+console.log(binary.search(binary.root, 6));
+console.log(binary.search(binary.root, 7));
+console.log(binary.search(binary.root, 2));
